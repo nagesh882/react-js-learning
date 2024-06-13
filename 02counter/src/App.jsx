806cdcad92583b98e486fd2c40e9f65c1d381f03
Nagesh <yashchavan192001@gmail.com>
2024-06-14 04:49:52 +0530

    Assignment:- when counter reached 20 then it not be added and when counter reached 0 then it not be less than 0 or minus value

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -10,18 +10,51 @@ function App() {
 
   // let counter = 5
 
+  // const addValue = () => {
+  //   // counter = counter + 1;
+  //   setCounter(counter + 1)
+  //   console.log("value added", counter);
+  // }
+
+
+  // const removeValue = () => {
+  //   // counter = counter - 1
+  //   setCounter(counter - 1)
+  //   console.log("value removed", counter);
+  // }
+
+
+
+  
+  // Assignment
+  // when counter reached 20 then it not be added
+  // when counter reached 0 then it not be less than 0 or minus value
+
   const addValue = () => {
-    // counter = counter + 1;
-    setCounter(counter + 1)
-    console.log("value added", counter);
+
+    if (counter < 20) {
+      const newCounter = counter + 1;
+      setCounter(newCounter);
+      console.log("Couter value", newCounter);
+    } else {
+      console.log("Counter reached 20");
+    }
+
   }
 
 
   const removeValue = () => {
-    // counter = counter - 1
-    setCounter(counter - 1)
-    console.log("value removed", counter);
+
+    if (counter > 0) {
+      const newCounter = counter - 1;
+      setCounter(newCounter);
+      console.log("Removed couter", newCounter)
+    } else {
+      console.log("Couter value is less than 0")
+    }
   }
+
+
 
   return (
     <>
